0c454be7025814f47576497ef0ba8d6a25a0baad
MI Shanto <shanto4045@gmail.com>
2022-11-22 00:32:35 +0600

    chemical updated

--- Chemical_Reaction.py
+++ Chemical_Reaction.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 k1 = 0.008
 k2 = 0.002
-a = 100
-b = 50
-c = 0
+a0 = 100
+b0 = 50
+c0 = 0
 delta_t = 0.1
 t = 0
 n = input()
@@ -14,26 +14,30 @@ array_b = []
 array_c = []
 array_t = []
 
-while t < int(n):
+print("\tTime\t \tA (i)\t \tB(i)\t \tC(i)\t")
+print("%12.2f %16.2f %15.2f %15.2f"%(t,a0,b0,c0))
 
+while t <= int(n):
     t += delta_t
 
-    array_a.append(a)
-    a = a + (k2 * c - k1 * a * b) * delta_t
+    ai = a0 + (k2 * c0 - k1 * a0 * b0) * delta_t
+    bi = b0 + (k2 * c0 - k1 * a0 * b0) * delta_t
+    ci = c0 + (2 * k1 * a0 * b0 - 2 * k2 * c0) * delta_t
 
-    array_b.append(b)
-    b = b + (k2 * c - k1 * a * b) * delta_t
+    a0 = ai
+    b0 = bi
+    c0 = ci
 
-    array_c.append(c)
-    c = c + (2 * k1 * a * b - 2 * k2 * c) * delta_t
-
+    array_a.append(ai)
+    array_b.append(bi)
+    array_c.append(ci)
     array_t.append(t)
+   
+    print("%12.2f %16.2f %15.2f %15.2f"%(t,ai,bi,ci))
     
-print(array_a, array_b, array_c)
-
-plt.plot(array_t, array_a, label="a", color="red")
-plt.plot(array_t, array_b, label = "b", color = "green")
-plt.plot(array_t, array_c, label = "c", color = "blue")
+plt.plot(array_t, array_a, label="a", color="Red")
+plt.plot(array_t, array_b, label="b", color="green")
+plt.plot(array_t, array_c, label="c", color="blue")
 
 plt.legend()
 
